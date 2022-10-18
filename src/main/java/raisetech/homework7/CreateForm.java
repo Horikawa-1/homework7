6fb96232bcfce25d577a95d0deee39993669f87f
kaidou2 <rooftop@ymail.ne.jp>
2022-10-18 21:59:43 +0900

    こみっと

---
+++ src/main/java/raisetech/homework7/CreateForm.java
@@ -0,0 +1,11 @@
+package raisetech.homework7;
+
+public class CreateForm {
+  private String name;
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+}
