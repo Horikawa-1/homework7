2e5ee9dc518639172acfb6ce2bbd7199473d4206
kaidou2 <111167638+kaidou2@users.noreply.github.com>
2022-11-16 08:56:11 +0900

    Update RestController.java

--- homework7/src/main/java/raisetech/homework7/RestController.java
+++ homework7/src/main/java/raisetech/homework7/RestController.java
@@ -37,10 +37,9 @@ public class RestController {
 
   //PATCHメソッドはPOSTと同じく入力した文字列が空文字、null、20文字以上なら受け取らないで、URLパラメータの「names/」の後に整数を入れる仕様
   @PatchMapping("/names/{id}")
-  public ResponseEntity<Map<String, String>> updateUser(@PathVariable("id") int id, @RequestBody @RequestParam(value = "name") String name, @Validated UserForm form, UriComponentsBuilder builder) {
-    String idNumber = String.valueOf(id);
+  public ResponseEntity<Map<String, String>> updateUser(@PathVariable("id") String id, @RequestBody @RequestParam(value = "name") String name, @Validated UserForm form, UriComponentsBuilder builder) {
 
-    String path = "/names/" + idNumber + form.getName();
+    String path = "/names/" + form.getId() + form.getName();
 
     URI url = builder.path(path).build().toUri();
 
