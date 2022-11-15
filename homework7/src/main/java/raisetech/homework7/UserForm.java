767e19162981d26ca898f819e84f643cec5e81f5
kaidou2 <111167638+kaidou2@users.noreply.github.com>
2022-11-15 11:19:37 +0900

    Update UserForm.java

--- homework7/src/main/java/raisetech/homework7/UserForm.java
+++ homework7/src/main/java/raisetech/homework7/UserForm.java
@@ -1,10 +1,9 @@
 package raisetech.homework7;
 
-import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public class UserForm {
-  @NotNull
+
   @Length(min=8, max=20 )
   private String name;
   public String getName() {
