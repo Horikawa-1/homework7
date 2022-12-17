e74e6a993c81bfafc24a2c8bc820c8c8fc9234ef
Horikawa-1 <111167638+Horikawa-1@users.noreply.github.com>
2022-12-18 00:50:54 +0900

    Update UserForm.java
    無駄な空白行を消し、
    setNameメソッドはほかのクラスで全く使わないので削除しました。

--- homework7/src/main/java/raisetech/homework7/UserForm.java
+++ homework7/src/main/java/raisetech/homework7/UserForm.java
@@ -9,13 +9,7 @@ public class UserForm {
   @Length(max = 19)
   private String name;
 
-
   public String getName() {
     return name;
-  }
-
-
-  public void setName(String name) {
-    this.name = name;
   }
 }
